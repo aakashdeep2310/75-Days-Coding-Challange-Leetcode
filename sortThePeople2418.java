940ba0776120b93492ac75c079b2ffe27362f95f
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-07-26 02:21:06 +0530

    leetcode

---
+++ sortThePeople2418.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+       int n = heights.length;
+       Integer index[] = new Integer[n];
+
+       for(Integer i = 0; i < n; i++){
+            index[i] = i;
+        }
+
+        Arrays.sort(index, new Comparator<Integer>(){
+            public int compare(Integer a, Integer b){
+                return heights[b] - heights[a];
+            }
+        });
+
+        String res[] = new String[n];
+        int i = 0;
+        for(Integer ind : index){
+            res[i] = names[ind];
+            i++;
+        }
+        return res;
+    }
+}
