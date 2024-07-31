1235eaaaaa087dab46fb7b0f9f34c94c3ae7a808
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-08-01 02:12:53 +0530

    leetcode

---
+++ powerOfTwo.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+       
+        if(n == 1){
+            return true;
+        }
+        if( n== 0){
+            return false;
+        }
+      
+
+    boolean ans = false;
+        if(n%2==0 || n == 1){
+             ans = isPowerOfTwo(n/2);
+        }
+        if(!ans){
+            return false;
+        }
+        return true;
+        // return false;
+    }
+}
+
+public class powerOfTwo {
+    
+}
