024283b98673be6d0b6e5c3ddd410d02741a2820
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2024-11-11 19:12:04 +0530

    2601. Prime Subtraction Operation

---
+++ PrimeSubtractionOperation.java
@@ -0,0 +1,47 @@
+class Solution {
+    static boolean isPrime[] = new boolean[1000];
+    public boolean primeSubOperation(int[] nums) {
+        int n = nums.length;
+
+        sieve();
+
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                continue;
+            }
+
+            for(int p = 2; p < nums[i]; p++){
+                if(!isPrime[p]){
+                    continue;
+                }
+
+                if(nums[i] - p < nums[i+1]){
+                    nums[i] -= p;
+                    break;
+                }
+
+            }
+            if(nums[i] >= nums[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void sieve(){
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i = 2; i * i < 1000; i++){
+            if(isPrime[i] == true){
+                for(int j = i*i; j<1000; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+
+
+        
+}
